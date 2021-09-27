6173fde534eacfa09fae2824d91356fd8a60cad7
Johnny Faria <johnnys.faria@gmail.com>
2021-09-27 15:33:04 -0300

    responsivo

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -5,32 +5,30 @@ import { NavLink } from 'react-router-dom';
 
 const Header = ({ label, placeholder }) => {
   return (
-    <>
-      <nav className={'nav-container'}>
-        <NavLink to="/">
-          <div className={'nav-home'}>Healthy Food</div>
-        </NavLink>
+    <nav className={'nav-container'}>
+      <NavLink to="/">
+        <div className={'nav-home'}>Healthy Food</div>
+      </NavLink>
 
-        <div className={'nav'}>
-          <ul className={'nav-lista'}>
-            <li>
-              <a>HEALTHY RECIPES</a>
-            </li>
-            <li>
-              <a>BLOG</a>
-            </li>
-            <li>
-              <a>JOIN</a>
-            </li>
-          </ul>
-          <nav>
-            <NavLink to="/Register" activeStyle={{ textDecoration: 'none' }}>
-              <Botao label={'Register'} branco />
-            </NavLink>
-          </nav>
-        </div>
-      </nav>
-    </>
+      <div className={'nav'}>
+        <ul className={'nav-lista'}>
+          <li>
+            <a>HEALTHY RECIPES</a>
+          </li>
+          <li>
+            <a>BLOG</a>
+          </li>
+          <li>
+            <a>JOIN</a>
+          </li>
+        </ul>
+        <nav>
+          <NavLink to="/Register" activeStyle={{ textDecoration: 'none' }}>
+            <Botao label={'Register'} branco />
+          </NavLink>
+        </nav>
+      </div>
+    </nav>
   );
 };
 
